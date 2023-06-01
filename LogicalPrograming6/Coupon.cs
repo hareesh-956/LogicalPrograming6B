54957ace07f319e50fcb59b934134ddf358d93e5
hareesh <hareesh.vanapalli.888@gmail.com>
2023-06-01 19:29:45 +0530

    [Hareesh]

---
+++ LogicalPrograming6/Coupon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograming6
+{
+    internal class Coupon
+    {
+        public class CouponNumberSimulator
+        {
+            public static int GenerateDistinctCouponNumbers(int N)
+            {
+                Random random = new Random();
+                List<int> distinctCoupons = new List<int>();
+                int randomNumbersGenerated = 0;
+
+                while (distinctCoupons.Count < N)
+                {
+                    int randomNumber = random.Next(1, N + 1);
+                    randomNumbersGenerated++;
+
+                    if (!distinctCoupons.Contains(randomNumber))
+                    {
+                        distinctCoupons.Add(randomNumber);
+                        Console.WriteLine($"Generated distinct coupon number: {randomNumber}");
+                    }
+                }
+
+                return randomNumbersGenerated;
+            }
+        }
+
+        
+            
+        
+    }
+}
--- LogicalPrograming6/Program.cs
+++ LogicalPrograming6/Program.cs
@@ -1,11 +1,24 @@
-﻿namespace LogicalPrograming6
+﻿using System;
+using static LogicalPrograming6.Coupon;
+
+namespace LogicalPrograming6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        
             Revers.ReverseNumber();
+            Console.Write("Enter the number of distinct coupon numbers: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int totalRandomNumbersNeeded = CouponNumberSimulator.GenerateDistinctCouponNumbers(N);
+            Console.WriteLine($"Total random numbers needed to have all distinct numbers: {totalRandomNumbersNeeded}");
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
