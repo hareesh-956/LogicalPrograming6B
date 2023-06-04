c959524a294a279c228dad0270ea8c2e99228825
hareesh <hareesh.vanapalli.888@gmail.com>
2023-06-04 20:44:26 +0530

    [Hareesh]

---
+++ LogicalPrograming6/MonthlyPayment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograming6
+{
+    internal class MonthlyPayment
+    {
+        public static double CalculateMonthlyPayment(double P, double Y, double R)
+        {
+            // Number of monthly payments
+            double n = 12 * Y;
+
+            // Monthly interest rate
+            double r = R / (12 * 100);
+
+            // Calculate the monthly payment
+            double payment = (P * r) / (1 - Math.Pow(1 + r, -n));
+
+            return payment;
+        }
+
+    }
+}
+
--- LogicalPrograming6/Program.cs
+++ LogicalPrograming6/Program.cs
@@ -30,9 +30,44 @@ namespace LogicalPrograming6
                 ts.Milliseconds / 10);
             Console.WriteLine("Elapsed Time: " + elapsedTime);*/
 
-            Console.WriteLine("Enter the amount of change required:");
-            int change = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("₹ " + note);
+            // Console.WriteLine("Enter the amount of change required:");
+            // int change = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(">>>\tYou selected Calculate Monthly Payment");
+            Console.WriteLine("Enter the (Y) years to pay off: ");
+            int P = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the (P) principal loan amount: ");
+            int Y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the (R) percent interest: ");
+            int R = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The Monthly Payment AS PER THE GIVEN FORMULA is:" + MonthlyPayment.CalculateMonthlyPayment(P, Y, R));
+            // Implement Calculate Monthly Payment logic here
+            Console.WriteLine();
+
+            Console.WriteLine(">>>\tYou selected Day of Week");
+            /*int day = 0;
+            int month = 0;
+            int year = 0;*/
+            Console.WriteLine("Enter the date: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the month (in Number): ");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            int dayOfWeek = DayOfWeek.DayWeek(day, month, year);
+            string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            string dayName = days[dayOfWeek]; // dayOfWeek is the index into the array
+
+            Console.WriteLine("The day of the week on the given date (" + day + "/" + month + "/" + year + ") is: " + dayOfWeek + " which is " + dayName + ".");
+            // Implement Day of Week logic here
+            Console.WriteLine();
+
 
 
         }
